abc7518a12f0a70d3333fdbbd7ccba2a67bc81d2
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-27 21:37:47 +0530

    Update MyToken.sol

--- Foundry-ERC20/script/DeployMyToken.sol
+++ Foundry-ERC20/script/DeployMyToken.sol
@@ -1,6 +1,8 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// //SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.18;
 
-//import {Script, console} from "forge-std/Script.sol";
-import {MyToken} from "../src/MyToken.sol";
+// import "forge-std/Script.sol";
+// import {MyToken} from "../src/MyToken.sol";
 
+// contract DeployMyToken is Script {
+// }
