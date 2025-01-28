03d36d942e7adf2a249e9ac7f7acaf965c6f003a
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-29 04:47:20 +0530

    Deploy

---
+++ Foundry-nft/script/interactions.s.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "forge-std/Script.sol";
+import { DevOpsTools } from "lib/foundry-devops/src/DevOpsTools.sol";
+
+
+contract MintBasicNFT is Script {
+
+    function run() external {
+
+    }
+}
