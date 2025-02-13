eb3e8d863fa3d76a2cb1c69c54788564eb7428cf
Ashiq Ahamed <aashiq121205@gmail.com>
2025-02-14 04:10:15 +0530

    RebaseTokenPool.sol

--- Rebase-token/src/RebaseTokenPool.sol
+++ Rebase-token/src/RebaseTokenPool.sol
@@ -2,12 +2,3 @@
 pragma solidity ^0.8.24;
 
 import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
-
-contract RebaseTokenPool is TokenPool {
-
-    constructor(IERC20 _token, address[] memory _allowList, address _rmnProxy, address _router)
-        TokenPool(_token, 18, _allowList, _rmnProxy, _router) {}
-
-
-        
-}
