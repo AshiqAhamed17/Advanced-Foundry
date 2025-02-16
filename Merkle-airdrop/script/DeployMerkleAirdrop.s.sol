fea700b5aa1616217861d3a5e9493d892163dc51
Ashiq Ahamed <aashiq121205@gmail.com>
2025-02-16 17:38:38 +0530

    Script: DeployMerkleAirdrop

---
+++ Merkle-airdrop/script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { MerkelAirdrop } from "../src/MerkleAirdrop.sol";
+import { Script } from "forge-std/Script.sol";
+import { BagelToken } from "../src/BagelToken.sol";
+import { console } from "forge-std/console.sol";
+
+contract DeployMerkleAirdrop is Script {
+
+}
